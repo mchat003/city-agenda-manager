ab1536d3c94362eafdbde18fa92d35f28074f81b
Miguel A. Chateloin <mchat003@fiu.edu>
2016-12-29 01:07:41 -0500

    complete 3/4 api endpoints

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Response;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,13 +14,134 @@ use Illuminate\Http\Response;
 |
 */
 
+//$currentItemPath = env('ITEM_UNDER_DISCUSSION_FILE_PATH');
+//$meetingsPath = env('MEETING_AGENDA_FILE_PATH');
+
+
+function updateMeetingStatus($id, $status){
+	$meetingsPath = '../' . env('MEETING_AGENDA_FILE_PATH');;
+	$reading = fopen($meetingsPath, 'r');
+	$writing = fopen($meetingsPath . '.tmp', 'w');
+
+	$replaced = false;
+	$index = 0;
+
+	while (!feof($reading)) {
+	  $line = fgets($reading);
+	  if ($index == $id) {
+
+	  	$statusPos = getStatusPos($line);
+
+		if($statusPos !== false){
+			$line = trim(substr($line, 0, $statusPos)) . ' ' . substr($line, $statusPos);
+		} else {
+			$line = $line . ' ' . substr($line, $statusPos);
+		}
+
+	    $replaced = true;
+	  }
+	  
+	  fputs($writing, $line);
+	  $index++;
+	}
+	fclose($reading); fclose($writing);
+	// might as well not overwrite the file if we didn't replace anything
+	if ($replaced) 
+	{
+	  rename($meetingsPath . '.tmp', $meetingsPath);
+	} else {
+	  unlink($meetingsPath . '.tmp');
+	}
+}
+
+function getStatusPos($text){
+	$statuses = array(
+		'DISCUSSED'
+	);
+
+
+	foreach($statuses as $status){
+		
+		$statusPos = strpos($text, $status);
+
+		if($statusPos !== false){
+			return $statusPos;
+		}
+	}
+
+	return false;
+}
+
 $app->get('/', function () use ($app) {
     return $app->version();
 });
 
-$app->get('config', function () {
+$app->get('api/config', function () {
     return response()->json([
-    	'itemUnderDiscussionFilePath' => env('ITEM_UNDER_DISCUSSION_FILE_PATH'),
-    	'meetingAgendaFilePath' => env('MEETING_AGENDA_FILE_PATH')
+    	'itemUnderDiscussionFilePath' => $currentItemPath,
+    	'meetingAgendaFilePath' => $meetingsPath
 	]);
+});
+
+
+
+$app->get('api/meetings', function () {
+	$meetingsPath = env('MEETING_AGENDA_FILE_PATH');
+	$meetingsFileContents = file_get_contents('../' . $meetingsPath, FILE_USE_INCLUDE_PATH);
+	$meetingList = explode(PHP_EOL, $meetingsFileContents);
+	$body = array();
+
+	foreach($meetingList as $index => $line){
+		if(!empty($line)){
+
+			$statusPos = getStatusPos($line);
+
+			if($statusPos !== false){
+				$body[$index]['title'] = trim(substr($line, 0, $statusPos));
+				$body[$index]['status'] = substr($line, $statusPos);
+			} else {
+				$body[$index]['title'] = $line;
+				$body[$index]['status'] = '';
+			}
+
+			$body[$index]['id'] = $index;
+			
+		}
+	}
+
+    return response()->json($body);
+});
+
+
+$app->put('api/meetings/{index}', function () {
+	//$meetingsPath = env('MEETING_AGENDA_FILE_PATH');
+	//$newStatus = $request->json('status');
+
+	//updateMeetingStatus($id, $newStatus);
+
+    return response()->json([
+    	'index' => '$index',
+    	'status' => '$newStatus'
+    ]);
+});
+
+
+$app->get('api/meetings/current', function () {
+	$currentItemPath = env('ITEM_UNDER_DISCUSSION_FILE_PATH');
+	$currentItemFileContents = file_get_contents('../' . $currentItemPath, FILE_USE_INCLUDE_PATH);
+
+    return response()->json([
+    	'title' => trim($currentItemFileContents)
+    ]);
+});
+
+$app->put('api/meetings/current', function (Request $request) {
+	$currentItemPath = env('ITEM_UNDER_DISCUSSION_FILE_PATH');
+	$newTitle = $request->json('title');
+
+	file_put_contents('../' . $currentItemPath, $newTitle);
+
+    return response()->json([
+    	'title' => $newTitle
+    ]);
 });
